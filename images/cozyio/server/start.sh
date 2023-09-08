2e754d43f947840b11825d29dd3ad37ac7b3edfa
Dmitry Bardyshev <brd.dmitry@gmail.com>
2023-09-08 22:50:26 +0300

    fix

--- images/cozyio/server/start.sh
+++ images/cozyio/server/start.sh
@@ -1,3 +1,3 @@
 sh /etc/create-config.sh
 
-parallel -j 3 -- '/etc/start-db.sh' 'sudo -u cozy-stack cozy-stack serve' 'sh /etc/create-instance.sh'
+parallel -j 3 -- 'sh /etc/start-db.sh' 'sudo -u cozy-stack cozy-stack serve' 'sh /etc/create-instance.sh'
