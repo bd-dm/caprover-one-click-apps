721ace0c3e6e6df21331c70899cd6a3905bb3447
Dmitry Bardyshev <brd.dmitry@gmail.com>
2023-09-10 22:40:52 +0300

    backup folders

--- images/restic/start.sh
+++ images/restic/start.sh
@@ -7,8 +7,14 @@ else
 fi
 
 # Create crontab file
+BACKUP_FOLDERS_ARRAY=($(echo $BACKUP_FOLDERS | tr "," "\n"))
+echo "Backup folders: ${BACKUP_FOLDERS}"
 touch /etc/crontabs/root/cronjobs
-echo "${CRON_BACKUP_SCHEDULE} restic backup /backup" >> /etc/crontabs/root/cronjobs
+
+for folder in "${!BACKUP_FOLDERS_ARRAY[@]}"
+do
+  echo "${CRON_BACKUP_SCHEDULE} restic backup /host/${folder}" >> /etc/crontabs/root/cronjobs
+done
 echo "${CRON_CLEANUP_SCHEDULE} restic forget --keep-last ${KEEP_BACKUPS}" >> /etc/crontabs/root/cronjobs
 
 crond -f -d 8
